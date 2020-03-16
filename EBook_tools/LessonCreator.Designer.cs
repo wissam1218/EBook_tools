667093c75d745952b460d8e4cbdf48a51f596bfa
Peter Richter <prichte2@emich.edu>
2020-03-16 17:10:41 -0400

    Added an area to upload video, needs a place that fits

--- EBook_tools/LessonCreator.Designer.cs
+++ EBook_tools/LessonCreator.Designer.cs
@@ -28,10 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.viewPDFBtn = new System.Windows.Forms.Button();
             this.testsMenuBtn = new System.Windows.Forms.Button();
             this.editPDFBtn = new System.Windows.Forms.Button();
             this.backBtn = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.picButton = new System.Windows.Forms.Button();
+            this.image1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.image1)).BeginInit();
             this.SuspendLayout();
             // 
             // viewPDFBtn
@@ -53,6 +59,7 @@
             this.testsMenuBtn.Size = new System.Drawing.Size(101, 25);
             this.testsMenuBtn.TabIndex = 2;
             this.testsMenuBtn.Text = "Pre/Post tests";
+            this.toolTip1.SetToolTip(this.testsMenuBtn, "Create a Pretest or Posttest for your lesson");
             this.testsMenuBtn.UseVisualStyleBackColor = true;
             this.testsMenuBtn.Click += new System.EventHandler(this.testBtn_Click);
             // 
@@ -69,7 +76,7 @@
             // 
             // backBtn
             // 
-            this.backBtn.Location = new System.Drawing.Point(446, 257);
+            this.backBtn.Location = new System.Drawing.Point(761, 411);
             this.backBtn.Name = "backBtn";
             this.backBtn.Size = new System.Drawing.Size(75, 23);
             this.backBtn.TabIndex = 5;
@@ -77,11 +84,41 @@
             this.backBtn.UseVisualStyleBackColor = true;
             this.backBtn.Click += new System.EventHandler(this.backBtn_Click);
             // 
+            // toolTip1
+            // 
+            this.toolTip1.Popup += new System.Windows.Forms.PopupEventHandler(this.toolTip1_Popup);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // picButton
+            // 
+            this.picButton.Location = new System.Drawing.Point(65, 359);
+            this.picButton.Name = "picButton";
+            this.picButton.Size = new System.Drawing.Size(101, 25);
+            this.picButton.TabIndex = 7;
+            this.picButton.Text = "Upload Image";
+            this.picButton.UseVisualStyleBackColor = true;
+            this.picButton.Click += new System.EventHandler(this.picButton_Click);
+            // 
+            // image1
+            // 
+            this.image1.Location = new System.Drawing.Point(51, 193);
+            this.image1.Name = "image1";
+            this.image1.Size = new System.Drawing.Size(129, 122);
+            this.image1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.image1.TabIndex = 8;
+            this.image1.TabStop = false;
+            // 
             // LessonCreator
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(533, 292);
+            this.ClientSize = new System.Drawing.Size(848, 446);
+            this.Controls.Add(this.image1);
+            this.Controls.Add(this.picButton);
             this.Controls.Add(this.backBtn);
             this.Controls.Add(this.editPDFBtn);
             this.Controls.Add(this.testsMenuBtn);
@@ -89,6 +126,7 @@
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "LessonCreator";
             this.Text = "Lesson Creator";
+            ((System.ComponentModel.ISupportInitialize)(this.image1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -99,6 +137,10 @@
         private System.Windows.Forms.Button testsMenuBtn;
         private System.Windows.Forms.Button editPDFBtn;
         private System.Windows.Forms.Button backBtn;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button picButton;
+        private System.Windows.Forms.PictureBox image1;
     }
 }
 
--- EBook_tools/LessonCreator.cs
+++ EBook_tools/LessonCreator.cs
@@ -43,5 +43,29 @@ namespace EBook_tools
             page.Show();
 
         }
+
+        private void toolTip1_Popup(object sender, PopupEventArgs e)
+        {
+
+        }
+
+        private void picButton_Click(object sender, EventArgs e)
+        {
+            string imageLocation = "";
+            try {
+                OpenFileDialog dialog = new OpenFileDialog();
+                dialog.Filter = "jpg files(*.jpg)|*.jpg| PNG files(*.png)|*.png| All Files(*.*)|*.*";
+
+                if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK) {
+                    imageLocation = dialog.FileName;
+
+                    image1.ImageLocation = imageLocation;
+                }
+
+            }
+            catch (Exception){
+                MessageBox.Show("An Error Ocurred", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
     }
 }
